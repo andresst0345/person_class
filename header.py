151b9e58ee063426985d65c7f1f4c90a94941c6c
andresst0345 <juan.salazar222@tau.usbmed.edu.co>
2023-05-11 13:57:42 -0500

    se creo la clase persona con el  constructor

---
+++ header.py
@@ -0,0 +1,10 @@
+
+
+class persona ():
+
+    def __init__(self,nombre='', edad=0, estatura=0.0 ) -> None:
+        self.nombre = nombre
+        self.edad = edad
+        self.estatura = estatura
+
+    
